01e5f89ed9c07a1e64073826a487929100d83084
LovelaceEngine <227027502+LovelaceEngine@users.noreply.github.com>
2025-10-08 23:45:25 +0300

    Новый тест: вводит ответ и проверяет соответсвие значений

---
+++ test_product_page.py
@@ -0,0 +1,26 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.product_button_should_be_click()
+    page.solve_quiz_and_get_code()
+    page.check_product_parameter(*ProductPageLocators.PRODUCT_PRICE, *ProductPageLocators.PRODUCT_PRICE_ADDED)
+    page.check_product_parameter(*ProductPageLocators.PRODUCT_NAME, *ProductPageLocators.PRODUCT_NAME_ADDED)
+    time.sleep(3)
