715d601edc7531d078190c2334134af28d70e137
LovelaceEngine <227027502+LovelaceEngine@users.noreply.github.com>
2025-10-08 23:42:07 +0300

    Добавил математическую функцию для теста

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
+import math
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
@@ -19,3 +21,17 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
