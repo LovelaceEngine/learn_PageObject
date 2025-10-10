620b8ae2ba9338fa370083ba7222f34264619251
LovelaceEngine <227027502+LovelaceEngine@users.noreply.github.com>
2025-10-10 23:23:33 +0300

    Новый тест: переход в корзину и проверка содержимого

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,17 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
 
-
+@pytest.mark.xfail
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
@@ -16,7 +19,14 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.basket_button_click()
+    page.is_basket_empty()
+
