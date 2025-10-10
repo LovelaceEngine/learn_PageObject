fec0d44c22935df4bd315efb0ce7a7febefeeecc
LovelaceEngine <227027502+LovelaceEngine@users.noreply.github.com>
2025-10-10 23:24:06 +0300

    Новый тест: переход в корзину и проверка содержимого

--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 from .pages.locators import ProductPageLocators
+from .pages.basket_page import BasketPage
 
 
 @pytest.mark.parametrize('link', [#"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -40,3 +41,10 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.basket_button_click()
+    page.is_basket_empty()
