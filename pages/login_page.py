32ce6a48c371a25627832887b1a30c021031eb8c
LovelaceEngine <227027502+LovelaceEngine@users.noreply.github.com>
2025-10-18 01:20:10 +0300

    Добавил метод регистрации register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import BasePageLocators
 from .locators import LoginPageLocators
@@ -18,3 +20,10 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not present"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_SUBMIT_BUTTON).click()
